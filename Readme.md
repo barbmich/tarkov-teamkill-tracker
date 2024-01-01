12bfb69745fa24c93fb4045db455f8e59ae520a8
Michele Barbiero <barbmich.since86@gmail.com>
2024-01-01 07:00:29 -0500

    add .env.example. update .gitignore. use env file in drizzle.config.ts. Create barebone Readme.md

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Tarkov Teamkill Tracker Bot
+
+This is a very simple bot that handles tracking the teamkills happened during raids, since we're so very good at the game.
+The code can be used by anyone, but you need to handle the setup yourself by creating your own application, and deploy it however you prefer.
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,6 @@
+import "./src/dotenv";
+import { env } from "./src/env";
 import type { Config } from "drizzle-kit";
-
-// this file doesn't support top-level await
-import dotenv from "dotenv";
-import path from "path";
-
-dotenv.config({
-    path: path.resolve(
-        process.cwd(),
-        `.env.${process.env.NODE_ENV ?? "development"}`
-    ),
-});
 
 export default {
     schema: "./src/db/schema.ts",
@@ -18,10 +9,6 @@ export default {
     verbose: true,
     strict: true,
     dbCredentials: {
-        host: process.env.PG_HOST!,
-        port: Number(process.env.PG_PORT!),
-        user: process.env.PG_USER!,
-        password: process.env.PG_PASSWORD!,
-        database: process.env.PG_DATABASE!,
+        connectionString: env.DATABASE_URL,
     },
 } satisfies Config;
